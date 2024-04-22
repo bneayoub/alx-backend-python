5cbd13cb64d1600c645e0ee5789543da329494c7
bneayoub <bne.ayoub@gmail.com>
2024-04-22 16:58:46 +0100

    Refactor task_wait_n function to execute task_wait_random n times in 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,7 +4,9 @@
 import asyncio
 from typing import List
 
+
 task_wait_random = __import__('3-tasks').task_wait_random
+
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''Executes task_wait_random n times.
